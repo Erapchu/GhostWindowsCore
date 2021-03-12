e71b6d88a81860eba8f6a20d00d2a78a008f3c6c
Andrey Nasonov <andrey.nasonov@actonica.ru>
2021-03-12 10:24:05 +0700

    Add project files.

---
+++ GhostWindowsCore/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GhostWindowsCore
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected async override void OnStartup(StartupEventArgs e)
+        {
+            var window1 = new MainWindow();
+            window1.Show();
+            await Task.Delay(1000);
+            window1.Hide();
+            var window2 = new MainWindow();
+            window2.Show();
+
+            // Then tap Ctrl + ALt + Del or Win + L
+
+            base.OnStartup(e);
+        }
+    }
+}
